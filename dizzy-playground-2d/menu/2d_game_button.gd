bf4acf894e3651ffc795c3c108260de6d8784465
Davis <davis.rogers@pdisoftware.com>
2026-06-19 10:16:46 -0500

    Playground init; added preview images on menu.

--- dizzy-playground-2d/menu/2d_game_button.gd
+++ dizzy-playground-2d/menu/2d_game_button.gd
@@ -1,5 +1,15 @@
 extends Button
 
+func _ready() -> void:
+	mouse_entered.connect(_on_mouse_entered)
+	mouse_exited.connect(_on_mouse_exited)
+
+func _on_mouse_entered() -> void:
+	%SurvivorPreview.visible = true
+
+func _on_mouse_exited() -> void:
+	%SurvivorPreview.visible = false
+
 func _pressed() -> void:
 	%DizzyTransitions.visible = true
 	%DizzyTransitions.transition_out_wipe_left()
