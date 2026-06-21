019448bf4932aad7e4e6ceb03ca023005edb417d
Dizzy <spiderllama@gmail.com>
2026-06-21 13:04:22 -0500

    fixed ASCII walks animation and turn left.

--- dizzy-playground-2d/boop_game/scripts/player.gd
+++ dizzy-playground-2d/boop_game/scripts/player.gd
@@ -13,17 +13,22 @@ const JUMP_VELOCITY = -400.0
 
 func _physics_process(delta: float) -> void:
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	if is_dead == true:
+	if is_dead:
 		velocity = direction * 0
 	else:
 		velocity = direction * 600
+	if direction.x < 0:
+		%ASCII.scale.x = -1
+	elif direction.x > 0:
+		%ASCII.scale.x = 1
+		
 	move_and_slide()
 	
-	if velocity.length() > 0.0 && is_dead == false:
+	if velocity.length() > 0.0 && not is_dead:
 		%ASCII.play_walk_animation()
-	if velocity.length() <= 0.0 && is_dead == false:
+	if velocity.length() <= 0.0 && not is_dead:
 		%ASCII.play_idle_animation()
-	if is_dead == true && death_anim == false:
+	if is_dead == true && not death_anim:
 		%ASCII.play_dead_animation()
 		death_anim = true
 	
