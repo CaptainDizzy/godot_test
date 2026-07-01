f72b61aa42cd95ae49316a4d5177eb96398faca2
Dizzy <spiderllama@gmail.com>
2026-07-01 15:49:47 -0500

    Fixed block and box bonking

--- dizzy-playground-2d/ascii/assets/block.gd
+++ dizzy-playground-2d/ascii/assets/block.gd
@@ -1,7 +1,7 @@
 extends StaticBody2D
 
-func _on_bonked(area: Area2D) -> void:
-	if area.is_in_group("player"):
+func _on_bonked(body: Node2D) -> void:
+	if body.is_in_group("player"):
 		%AnimationPlayer.play("break")
 		await get_tree().create_timer(0.5).timeout
 		queue_free()
