df448b07729999e6eed7ad14712792822459fcf0
Dizzy <spiderllama@gmail.com>
2026-07-02 18:06:26 -0500

    Fixed mob hitboxes; added randomized mob chars; made new tosser mob WIP

--- dizzy-playground-2d/ascii/scripts/crawler.gd
+++ dizzy-playground-2d/ascii/scripts/crawler.gd
@@ -7,6 +7,67 @@ var direction = 1
 var falling := false
 var is_dead := false
 
+func _ready() -> void:
+	var hair: String
+	var head: String
+	var c1 = floor(randi_range(0,5) + 0.5)
+	var c2 = floor(randi_range(0,8) + 0.5)
+	if c1 == 0:
+		%CrawlerBody/Body/Hair.text = "\""
+		%CrawlerBody/Body/Hair.position.y = -107
+	elif c1 == 1:
+		%CrawlerBody/Body/Hair.text = "\'"
+		%CrawlerBody/Body/Hair.position.y = -107
+	elif c1 == 2:
+		%CrawlerBody/Body/Hair.text = "|"
+		%CrawlerBody/Body/Hair.position.y = -107
+	elif c1 == 3:
+		%CrawlerBody/Body/Hair.text = "+"
+		%CrawlerBody/Body/Hair.position.y = -107
+		%StompBox/CBox.position.y = -100
+	elif c1 == 4:
+		%CrawlerBody/Body/Hair.text = "*"
+		%CrawlerBody/Body/Hair.position.y = -107
+		%StompBox/CBox.position.y = -100
+	elif c1 == 5:
+		%CrawlerBody/Body/Hair.text = "#"
+		%CrawlerBody/Body/Hair.position.y = -107
+	elif c1 == 6:
+		%CrawlerBody/Body/Hair.text = "~"
+		%CrawlerBody/Body/Hair.position.y = -97
+	elif c1 == 7:
+		%CrawlerBody/Body/Hair.text = "T"
+		%CrawlerBody/Body/Hair.position.y = -107
+	elif c1 == 8:
+		%CrawlerBody/Body/Hair.text = "x"
+		%CrawlerBody/Body/Hair.position.y = -107
+		%StompBox/CBox.position.y = -100
+	elif c1 == 9:
+		%CrawlerBody/Body/Hair.text = "X"
+		%CrawlerBody/Body/Hair.position.y = -107
+	elif c1 == 10:
+		%CrawlerBody/Body/Hair.text = "0"
+		%CrawlerBody/Body/Hair.position.y = -107
+		
+	if c2 == 0:
+		%CrawlerBody/Body/Bod.text = "o"
+	elif c2 == 1:
+		%CrawlerBody/Body/Bod.text = "x"
+	elif c2 == 2:
+		%CrawlerBody/Body/Bod.text = "e"
+	elif c2 == 3:
+		%CrawlerBody/Body/Bod.text = "c"
+	elif c2 == 4:
+		%CrawlerBody/Body/Bod.text = "*"
+	elif c2 == 5:
+		%CrawlerBody/Body/Bod.text = "w"
+	elif c2 == 6:
+		%CrawlerBody/Body/Bod.text = "s"
+	elif c2 == 7:
+		%CrawlerBody/Body/Bod.text = "H"
+	elif c2 == 8:
+		%CrawlerBody/Body/Bod.text = "|"
+
 func _physics_process(delta: float) -> void:
 	
 	# Add the gravity.
